ba8739513441ef44dc767c00367054a18dbd1a90
神﨑 翔一朗 <shoichiro.kanzaki@bizreach.co.jp>
2016-06-09 12:03:39 +0900

    ナビゲーション透過解除

--- Pod/Classes/TabPageOption.swift
+++ Pod/Classes/TabPageOption.swift
@@ -15,15 +15,11 @@ public struct TabPageOption {
     public var fontSize = UIFont.systemFontSize()
     public var currentColor = UIColor(red: 105/255, green: 182/255, blue: 245/255, alpha: 1.0)
     public var defaultColor = UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1.0)
-    public var tabBarAlpha: CGFloat = 0.95
+    public var tabBarAlpha: CGFloat = 1.0
     public var tabHeight: CGFloat = 32.0
     public var tabMargin: CGFloat = 20.0
     public var tabWidth: CGFloat?
-    public var tabBackgroundColor: UIColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.95)
-    public var pageBackgoundColor: UIColor = UIColor.whiteColor()
-     internal var tabBackgroundImage: UIImage {
-        return convertImage()
-    }
+    public var tabBackgroundColor: UIColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
 
     private func convertImage() -> UIImage {
         let rect : CGRect = CGRectMake(0, 0, 1, 1)
